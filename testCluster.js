f38a6399b5d6746b4ede303b8ad897d4341f4b2d
tauz-hub <tauasanto101@gmail.com>
2022-03-29 22:55:28 -0300

    creating request collection to workers

--- testCluster.js
+++ testCluster.js
@@ -2,23 +2,87 @@ import cluster from 'cluster'
 import os from 'os'
 const numCPUs = os.cpus().length;
 import http from 'http'
-if (cluster.isPrimary) {
-  console.log('Primary process rodando');
+import axios from 'axios'
 
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
+let i = 0
+async function requestRouter() {
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`Worker ${worker.process.pid} morreu, o código é: ${code}, e sinal: ${signal}`);
-    console.log('Iniciando um novo Worker');
-    cluster.fork();
-  });
-} else {
-  http.createServer((req, res) => {
-    res.writeHead(200);
-    res.end(`eu sou o worker ${process.pid}\n`);
-  }).listen(2000);
+  await axios.get("http://localhost:2000").then((req) => { console.log(i + " | " + req.data) })
 
-  console.log('Rodando na porta 2000 processo ' + process.pid);
+
 }
+
+async function requestRouterAnt() {
+
+  await axios.get("http://localhost:2000").then((req) => { console.log(i + " | " + req.data) })
+
+
+}
+
+async function criaCluster() {
+  if (cluster.isPrimary) {
+    console.log('Primary process rodando');
+
+    for (let i = 0; i < numCPUs; i++) {
+      cluster.fork();
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+      console.log(`Worker ${worker.process.pid} morreu, o código é: ${code}, e sinal: ${signal}`);
+      console.log('Iniciando um novo Worker');
+      cluster.fork();
+    });
+
+
+
+    setTimeout(() => {
+      requestRouterAnt()
+      requestRouterAnt()
+      requestRouterAnt()
+      requestRouterAnt()
+      requestRouterAnt()
+      requestRouterAnt()
+      i++
+    }, 4000)
+    setTimeout(() => {
+      requestRouterAnt()
+      requestRouterAnt()
+      requestRouterAnt()
+      requestRouterAnt()
+      requestRouterAnt()
+      requestRouterAnt()
+      i++
+    }, 8000)
+    setTimeout(() => {
+      requestRouterAnt()
+      requestRouterAnt()
+      requestRouterAnt()
+      requestRouterAnt()
+      requestRouterAnt()
+      requestRouterAnt()
+      requestRouterAnt()
+      i++
+    }, 12000)
+    setTimeout(() => {
+      requestRouterAnt()
+      requestRouterAnt()
+      requestRouterAnt()
+      requestRouterAnt()
+      requestRouterAnt()
+      requestRouterAnt()
+      requestRouterAnt()
+      i++
+    }, 16000)
+
+  } else {
+    http.createServer((req, res) => {
+      res.writeHead(200);
+      res.end(`eu sou o worker ${process.pid}\n`);
+    }).listen(2000);
+
+    console.log('Rodando na porta 2000 processo ' + process.pid);
+
+    await requestRouter()
+  }
+}
+await criaCluster()
